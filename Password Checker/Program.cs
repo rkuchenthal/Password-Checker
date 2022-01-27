d5c0e38a553666ad8b994310b9119cdf3e601539
Ryan kuchenthal <koochenthal0@gmail.com>
2022-01-26 17:00:30 -0800

    Add project files.

---
+++ Password Checker/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Password_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passScore = 0;
+            string lower = "abcdefghijklmnopqrstuvwxyz";
+            string upper = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+            string special = "!@#$%^&*~`?><";
+            string digits = "0123456789";
+            int minLength = 8;
+
+            //prompt user
+            Console.WriteLine("Enter password: ");
+            string pass = Console.ReadLine();
+
+            //check for min length
+            if (pass.Length > minLength) passScore++;
+            //check for upper case
+            if (Tools.Contains(pass, upper)) passScore++;
+            //check for lower case
+            if (Tools.Contains(pass, lower)) passScore++;
+            //check for special chars
+            if (Tools.Contains(pass, special)) passScore++;
+            //check for numbers
+            if (Tools.Contains(pass, digits)) passScore++;
+
+            //print score of password
+            switch (passScore)
+            {
+                case (0):
+                    Console.WriteLine("Password is very weak.");
+                    break;
+                case (1):
+                    Console.WriteLine("Password is weak.");
+                    break;
+                case (2):
+                    Console.WriteLine("Password is ok.");
+                    break;
+                case (3):
+                    Console.WriteLine("Password is good.");
+                    break;
+                case (4):
+                    Console.WriteLine("Password is very good.");
+                    break;
+                default:
+                    Console.WriteLine("Password is excellent.");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Password Checker/Tools.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Password_Checker
+{
+    public class Tools
+    {
+        public Tools()
+        {
+        }
+
+        public static bool Contains(string target, string list)
+        {
+            return target.IndexOfAny(list.ToCharArray()) != -1;
+        }
+
+       
+    }
+}
